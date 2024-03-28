e9110ca16f40a3a65442e44f3c6d7e283d3cae5d
igorgcampos <31486326+igorgcampos@users.noreply.github.com>
2024-03-28 14:32:39 -0300

    Update connection.py

--- src/connection.py
+++ src/connection.py
@@ -4,23 +4,14 @@ from loguru import logger
 from sqlalchemy import create_engine, text
 
 def get_database_url():
-    #db_user = os.getenv("DB_USER")
-    #db_pass = os.getenv("DB_PASS")
-    #db_name = os.getenv("DB_NAME")
-    #instance_connection_name = os.getenv("INSTANCE_CONNECTION_NAME")  # Identificador da instância Cloud SQL
-    #unix_socket_path = f"/cloudsql/{instance_connection_name}"  # Caminho correto do Unix socket
+    db_user = os.getenv("DB_USER")
+    db_pass = os.getenv("DB_PASS")
+    db_name = os.getenv("DB_NAME")
+    instance_connection_name = os.getenv("INSTANCE_CONNECTION_NAME")  # Identificador da instância Cloud SQL
+    unix_socket_path = f"/cloudsql/{instance_connection_name}"  # Caminho correto do Unix socket
     ##unix_socket_path = os.getenv("INSTANCE_CONNECTION_NAME")  # Formato '/cloudsql/project:region:instance'
-    #return f"mysql+pymysql://{db_user}:{db_pass}@/{db_name}?unix_socket={unix_socket_path}"
-    #def get_database_url():
-    db_user = "infograficos"
-    db_pass = "infograficos!@2024"
-    db_name = "infograficos"
-    public_ip = "35.230.182.174"
-    instance_connection_name = "infograficos-prd:us-east4:infograficos"  # Identificador da instância Cloud SQL
-    #unix_socket_path = f"/cloudsql/{instance_connection_name}"  # Caminho correto do Unix socket
-    #return f"mysql+pymysql://{db_user}:{db_pass}@/{db_name}?unix_socket={unix_socket_path}"
-    return f"mysql+pymysql://{db_user}:{db_pass}@{public_ip}/{db_name}"
-
+    return f"mysql+pymysql://{db_user}:{db_pass}@/{db_name}?unix_socket={unix_socket_path}"
+    
 def test_sqlalchemy_connection(database_url):
     try:
         engine = create_engine(database_url)
