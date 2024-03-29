2a1427c7ed052521a15b49c44ee3981d0bd1c76e
igorgcampos <31486326+igorgcampos@users.noreply.github.com>
2024-03-29 12:06:48 -0300

    Update connection.py

--- src/connection.py
+++ src/connection.py
@@ -12,7 +12,7 @@ def get_database_url():
     ##unix_socket_path = os.getenv("INSTANCE_CONNECTION_NAME")  # Formato '/cloudsql/project:region:instance'
     #return f"mysql+pymysql://{db_user}:{db_pass}@/{db_name}?unix_socket={unix_socket_path}"
     db_user = "infograficos"
-    db_pass = "infograficos!@2024"
+    db_pass = "123456"
     db_name = "infograficos"
     instance_connection_name = "infograficos-prd:us-east4:infograficos"  # Identificador da instância Cloud SQL
     unix_socket_path = f"/cloudsql/{instance_connection_name}"  # Caminho correto do Unix socket
